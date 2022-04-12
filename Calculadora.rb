f4d23806217208edf4c3a834ec4f684ba3f499f5
efsiqueira <edu.fss14@gmail.com>
2022-04-12 00:56:21 -0300

    Adicionando operações

--- Calculadora.rb
+++ Calculadora.rb
@@ -1,55 +1,75 @@
 class Calculadora
-    def initialize(first_value, second_value) 
-        @first_value = first_value
-        @second_value = second_value
+
+    def initialize(primeiro_valor, segundo_valor)
+        @primeiro_valor = primeiro_valor
+        @segundo_valor = segundo_valor
     end
-  
-    def soma 
-        @first_value + @second_value
+
+    def soma
+        @primeiro_valor + @segundo_valor
     end
-  
+
     def subtracao
-        @first_value - @second_value
+        @primeiro_valor - @segundo_valor
     end
-  
+
     def divisao
-        @first_value / @second_value
+        @primeiro_valor / @segundo_valor
     end
-  
+
     def multiplicacao
-        @first_value * @second_value
-    end
-    
+        @primeiro_valor * @segundo_valor
+    end    
+
     def porcentagem
-        (@first_value * @second_value) / 100
+        (@primeiro_valor / @segundo_valor) * 100
+    end
+
+    def modulo
+        @primeiro_valor % @segundo_valor
+    end
+
+    def potencia
+        @primeiro_valor ** @segundo_valor
+    end
+
+    loop do
+
+        puts "\nInforme qual operação deseja utilizar: (\n1 - Soma, \n2 - Subtração, \n3 - Divisão, \n4 - Multiplicação, \n5 - Porcentagem, \n6 - Módulo, \n7 - Potência, \n0 - Sair\n)"
+        operacao = gets.strip.to_i
+
+        break if operacao == 0
+
+        print "\nInforme o primeiro valor: "
+        primeiro_valor = gets
+
+        print "\nInforme o segundo valor: "
+        segundo_valor = gets
+
+        break if !numeros_validos
+
+        case operacao
+            when 1
+                operacao = :soma
+            when 2
+                operacao = :subtracao
+            when 3
+                operacao = :divisao
+            when 4
+                operacao = :multiplicacao
+            when 5
+                operacao = :porcentagem
+            when 6
+                operacao = :modulo
+            when 7
+                operacao = :potencia
+            else
+                puts "Insira um número entre 1 e 7"
+        end
+
+        calculadora = Calculadora.new(primeiro_valor.to_f, segundo_valor.to_f)
+        resultado = calculadora.public_send(operacao)
+        
+        puts "\nO resultado é: #{resultado}"
     end
 end
-
-puts "Informe qual operação deseja utilizar: (1 - Soma, 2 - Subtração, 3 - Divisão, 4 - Multiplicação, 5 - Porcentagem)"
-operacao = gets.chomp.to_i
-
-puts "Informe o primeiro valor: "
-first_value = gets.chomp
-
-puts "Informe o segundo valor: "
-second_value = gets.chomp
-
-case operacao
-    when 1
-        operacao = :soma
-    when 2
-        operacao = :subtracao
-    when 3
-        operacao = :divisao
-    when 4
-        operacao = :multiplicacao
-    when 5
-        operacao = :porcentagem
-    else
-    puts "Insira um número entre 1 e 5"
-end
-
-calculadora = Calculadora.new(first_value.to_f, second_value.to_f)
-resultado = calculadora.public_send(operacao)
-
-puts resultado
